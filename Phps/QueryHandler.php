8e076c9405d3f776621cc3a03ed2492861203cb4
Patricio P <patriciopenaojeda@gmail.com>
2023-11-28 13:48:50 -0600

    Final

--- Phps/QueryHandler.php
+++ Phps/QueryHandler.php
@@ -1,16 +1,21 @@
 <?php
+
+//Ubicamos la ruta de la Api
 define("EUROPEANAKEY", json_decode(file_get_contents("../Keys.json")));
 define("RUTA", "https://api.europeana.eu/record/v2/search.json?wskey=");
 
+
+
 class QueryHandler
 {
-
     private $queryString;
 
     public function __construct($queryString)
     {
         $this->queryString = $queryString;
     }
+
+//crea la consulta
 
     private function createQueryEP()
     {
@@ -22,7 +27,9 @@ class QueryHandler
             $tok = strtok(" ");
         }
 
-
+//Para las consultas de String (varias palabras) realiza la
+//busqueda utilizando el AND y realiza la query
+//Consulta (Palabra1 AND Palabra2)
         foreach ($String as $k => $v) {
             if ($k < sizeof($String) - 1) {
                 $completeQuery .= $v . "+AND+";
@@ -30,10 +37,12 @@ class QueryHandler
                 $completeQuery .= $v;
             }
         }
-
+//retorna la consulta completada
         return $completeQuery;
     }
 
+    //muestra la consulta en un nuevo documento, que en este caso será el Results
+    //De una página nueva de html
     public function showQuery()
     {
 
@@ -43,7 +52,7 @@ class QueryHandler
         $resultsContent = $htmlPage->getElementById("results");
         $resultList = $htmlPage->createElement('ul');
         $queryS = $this->createQueryEP();
-
+//key "hawlemono" fue otorgada por la Api para poder utilizar las busquedas
         $queryStr = http_build_query([
             'wskey' => "hawlemono",
             'query' => $queryS
@@ -54,10 +63,13 @@ class QueryHandler
         $results = file_get_contents($query);
         $obj = json_decode($results, true);
 
+        //por cada item de la busqueda se va a crear en el html 
+        //generamos un arreglo de estos items que son los resultados de la búsqueda
         foreach ($obj['items'] as $o) {
             $listItem = $htmlPage->createElement("li");
 //Crea una condición si el item tiene un "score" que lo muestre y lo multiplique *.5 para quedar en rango 0-100
             if (isset($o['score'])) {
+                //generamos el score o puntaje de la consulta
                 $o["score"] = $o["score"] *.5 ;
                 $listItem->textContent = $o["guid"] . " - SCORE: " . $o['score'];
             } else {
--- Phps/QueryResults.php
+++ Phps/QueryResults.php
@@ -1,9 +1,15 @@
 <?php
+
+
 include("QueryHandler.php");
 
-$query = $_POST["queryString"];
 
+
+$query = $_POST["queryString"];
 $queryH = new QueryHandler($query);
+//Muestra los resultados de la consulta que se realizó
 $queryH->showQuery();
+
+
 
 ?>
